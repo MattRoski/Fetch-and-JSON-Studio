8219c71806913a6c2a3979b470278ede0bd5abc3
MattRoski <41523468+MattRoski@users.noreply.github.com>
2022-03-10 20:11:17 -0600

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+    response.json().then(function(json) {
+       const destination = document.getElementById("destination");
+      // console.log(json);
+
+      let htmlOut = "";
+       for(let astronauts of json){
+           htmlOut += `<div class="astronaut">
+           <div class="bio">
+           <h3>${astronauts.firstName} ${astronauts.lastName} </h3>
+           <ul>
+            <li>Hours in space: ${astronauts.hoursInSpace}</li>
+            <li>Active: ${astronauts.active}</li>
+            <li>Skills: ${astronauts.skills}</li>
+         </ul>
+         </div>
+         <img class ="avatar" src="https://handlers.education.launchcode.org/static/images/${(astronauts.firstName).toLowerCase()}-${(astronauts.lastName).toLowerCase()}.jpg" />
+         </div>
+           `
+       }
+       container.innerHTML = htmlOut;
+        });
+    });
+ });
